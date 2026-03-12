37243b7b15dd56b3bc061e76e7d3d6e9245ced6d
Josh Albrecht <joshalbrecht@gmail.com>
2026-03-12 11:08:36 -0700

    ugh

--- libs/mng_recursive/imbue/mng_recursive/provisioning.py
+++ libs/mng_recursive/imbue/mng_recursive/provisioning.py
@@ -81,9 +81,11 @@ def _upload_deploy_files(
                     logger.debug("Skipping non-existent deploy file: {}", source)
                     continue
                 content = source.read_bytes()
-                futures.append(executor.submit(host.write_file, path=resolved_path, content=content))
+                # futures.append(executor.submit(host.write_file, path=resolved_path, content=content))
+                host.write_file(path=resolved_path, content=content)
             else:
-                futures.append(executor.submit(host.write_text_file, path=resolved_path, content=source))
+                # futures.append(executor.submit(host.write_text_file, path=resolved_path, content=source))
+                host.write_text_file(path=resolved_path, content=source)
 
             logger.trace("Uploaded deploy file: {} -> {}", dest_path, resolved_path)
             count += 1
