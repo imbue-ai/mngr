2919d27b50917361173a2912a19dee7428ec9eb9
Hynek Urban <hynek.urban@gmail.com>
2026-06-08 17:37:25 +0200

    Bump Latchkey to v2.15.2.

--- libs/mngr_latchkey/imbue/mngr_latchkey/remote_gateway.py
+++ libs/mngr_latchkey/imbue/mngr_latchkey/remote_gateway.py
@@ -25,7 +25,7 @@ from imbue.mngr_latchkey.store import permissions_path_for_host
 from imbue.mngr_latchkey.store import plugin_data_dir
 
 # Version of the upstream ``latchkey`` CLI to install on the VPS.
-LATCHKEY_VERSION: Final[str] = "2.15.1"
+LATCHKEY_VERSION: Final[str] = "2.15.2"
 
 # Port inside the container on which the VPS-resident gateway is reachable (the
 # VPS->container reverse tunnel binds it). Deliberately distinct from
